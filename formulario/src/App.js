177859266acaec9d173d4186c6cfc36dc91ad69a
nelsonamorim <nelsonamorim.dev@gmail.com>
2022-08-17 22:11:36 -0300

    modificado formulário para react

---
+++ formulario/src/App.js
@@ -0,0 +1,11 @@
+import './pages/FormularioMercado/style.css';
+import './pages/FormularioMercado/index'
+import FormularioMercado from './pages/FormularioMercado/index';
+
+function App() {
+  return (
+    <FormularioMercado/>
+  );
+}
+
+export default App;
